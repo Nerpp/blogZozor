7e552a09b4780f19260325af97a4c214f4b2647f
Nerpp <regiaurelien@gmail.com>
2019-03-15 17:15:48 +0100

    Debut sur le template Base

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * l'annotation Route
+     * Quand le navigateur appellera mon site.com /blog
+     * voici la fonction que tu dois appeler
+     * @Route("/blog", name="blog")
+     */
+    public function index()
+    {
+        //cela va retourner la vue du fichier template blog/index.html.twig
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    /**
+     * 
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        //le deuxieme parametre de la fonction render est un tableau avec la liste des variables que twig va utiliser
+        return $this->render('blog/home.html.twig',[
+            'title'=> 'Bienvenue sur le Blog de Zozor',
+            'age' => 31
+        ]);
+    }
+}
