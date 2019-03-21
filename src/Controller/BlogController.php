ba7d92985697cbc5e2b0e22372a7544c1671ba11
Nerpp <regiaurelien@gmail.com>
2019-03-21 13:57:21 +0100

    Ecriture des routes pour les articles

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController
 {
@@ -13,11 +15,18 @@ class BlogController extends AbstractController
      * voici la fonction que tu dois appeler
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
+        // repo va appeler la fonction getDoctrine en utilisant la classe Artice en tant que statique
+        // $repo = $this->getDoctrine()->getRepository(Article::class); n'est plus utile car je l'appelle dans la fonction index(ArticleRepository $repo)
+
+        $articles = $repo->findAll();
+
         //cela va retourner la vue du fichier template blog/index.html.twig
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            // je passe a twig les articles trouvés
+            'articles' => $articles
         ]);
     }
 
@@ -35,10 +44,21 @@ class BlogController extends AbstractController
     }
 
    /**
-    * @Route("/blog/12", name="blog_show")
+    * j'ecrit une route paramétré qui suivra l'id de l'article
+    * @Route("/blog/{id}", name="blog_show")
     */
-    public function show()
+    public function show(Article $article)
     {
-        return $this->render('blog/show.html.twig');
+        //$repo = $this->getDoctrine()->getRepository(Article::class);
+        //cette ligne est inutile car je l'ai injecter dans la fonction show(ArticleRepository $repo, $id)
+
+        //$article = $repo->find($id);
+        //je peu meme demander a Show de me charger l'id directement sans cette ligne grace au param converter https://symfony.com/doc/current/best_practices/controllers.html
+        //que je recupere de src/Entity/Article.php voir mes use
+        //Merci le service Container
+
+        return $this->render('blog/show.html.twig' , [
+            'article' => $article
+        ]);
     }
 }
